9251819dfd09b4ce82fb2b640dd145cde124db66
ptoulis <panos.toulis@gmail.com>
2013-12-02 00:40:41 -0500

    Simple logging facility. To be ctd

--- checks.R
+++ checks.R
@@ -3,22 +3,43 @@
 #
 # Simple checks for comparing objects (e.g. lists), including
 # logical checks, set checks and statistical tests.
+
 stop.now <- function(x, y, msg) {
   compact <- function(a) {
     if(is.vector(a))
       return(paste(head(a, min(length(a), 80)), collapse=", "))
   }
-  stop("-------\nDetails:\n",sprintf("[FAIL]: %s", paste(msg, collapse=" : ")), "\n",
-           sprintf("x = %s", compact(x)), "\n", 
-           sprintf("y = %s", compact(y)))
+  dump.frames()
+  calls <- names(last.dump)
+  calls <- sapply(calls, function(x) {
+    strsplit(x, ":")[[1]][1]
+  })
+  stop("\n>> ", sprintf("[FAIL]: %s", paste(msg, collapse=" : ")), "\n",
+       ">> Execution path: ", paste(calls, collapse=" -> "),
+        "\n>> Operands:\n", 
+        sprintf("x = %s...", compact(x)), "\n", 
+        sprintf("y = %s...", compact(y)))
 }
 
+CHECK_INPUT <- function(x, msg="n/a") {
+  if(length(x) < 1) {
+    stop.now(x, "n/a (ignore)", c("INPUT empty", msg))
+  }
+  if(any(is.na(x)))
+    stop.now(x, "n/a (ignore)", c("INPUT has NA", msg))
+  if(any(is.null(x)))
+    stop.now(x, "n/a (ignore)", c("INPUT has NULL", msg))
+}
 CHECK_TRUE <- function(x, msg="n/a") {
   if (!x)
     stop.now(x, "n/a (ignore)", c("Logical test failed", msg))
 }
 
 CHECK_EQ <- function(x, y, msg="n/a") {
+  CHECK_INPUT(x)
+  CHECK_INPUT(y)
+  if(length(x) != length(y))
+    stop.now(length(x), length(y), c("Not equal lengths", msg))
   if (!all(x==y)) stop.now(x, y, c("Arrays not equal.", msg)) 
 }
 
@@ -46,10 +67,11 @@ CHECK_MEMBER <- function(element, y, msg="n/a") {
   if (!all(is.element(element, y))) {
     stop.now(element, y, sprintf("Element not member of list : %s", msg))
   }
-  return(TRUE)
 }
 
 CHECK_NEAR <- function(x, y, tol=1e-2, msg="n/a") {
+  CHECK_INPUT(x)
+  CHECK_INPUT(y)
   if (any(abs(x-y) > tol))
     stop.now(x, y, c(sprintf("Failed at tolerance %.6f", tol), msg))
 }
@@ -66,6 +88,10 @@ CHECK_EXCEPTION <- function(expr, msg="n/a") {
 bootstrap.mean = function(x) sd(replicate(1000, { mean(sample(x, replace=T)) }))
 CHECK_MU0 <- function(x, mu0, msg="n/a") {
   # Will bootstrap x to see if mean(x) = mu0
+  if (!is.vector(x)) {
+    print(x)
+    stop("x needs to be a vector")
+  }
   boot.se = bootstrap.mean(x)
   CHECK_INTERVAL(mu0, min=mean(x) - 2 * boot.se, max=mean(x) + 2*boot.se,
                  msg=c("Not in bootstrap inteval", msg))
---
+++ logs.R
@@ -0,0 +1,48 @@
+## Log utilities.
+kLogFile <- "rtools-logs.txt"
+kCurrentLogLevel = 1
+#  FINE DEBUG  INFO  WARNING   ERROR
+#   0     1      2     3        4
+library(stringr)
+if (file.exists(file=kLogFile)) {
+  file.remove(file=kLogFile)
+}
+
+logthis <- function(x, level) {
+  wline <- function(vec) {
+    write(str_c(vec, collapse="\t"), file=kLogFile, append=T)
+  }
+  if (level >= kCurrentLogLevel) {
+    preamble <- sprintf("%s::", date())
+    if(is.character(x)) {
+      wline(c(preamble, x))
+    } else if(is.data.frame(x)) {
+      wline(c(preamble, "DATA FRAME"))
+      wline(c("\t", names(x)))
+      logthis(as.matrix(x), level=level)
+    } else if(is.matrix(x)) {
+      if(nrow(x) > 0)
+        for(i in 1:nrow(x))
+          wline(c("\t", as.vector(x[i, ])))
+    } else if (is.table(x)) {
+      wline(c(preamble, "TABLE"))
+      wline(names(x))
+      wline(as.numeric(x))
+    } else if(is.list(x)) {
+      wline(c(preamble, "LIST"))
+      wline(names(x))
+      for(i in names(x))
+        logthis(x[[i]], level)
+    } else if(is.vector(x)) {
+      wline(c(preamble, x))
+    } else {
+      wline(c(preamble, x))
+    }
+  }
+}
+
+logfine <- function(x) logthis(x, level=0)
+logdebug <- function(x) logthis(x, level=1)
+loginfo <- function(x) logthis(x, level=2)
+logwarning <- function(x) logthis(x, level=3)
+logerror <- function(x) logthis(x, level=4)
